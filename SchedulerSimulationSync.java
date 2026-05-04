88e45e3304dd0e097801fe4bf9f6521283336e21
arwa-bz <445052160@std.psau.edu.sa>
2026-05-05 01:08:51 +0300

    add cpu semaphore control in process execution then releasebit in finally block

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -122,7 +122,12 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
-
+        // Acquire CPU access before execution
+        try {
+            SharedResources.cpuSemaphore.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         try {
             if (startTime == -1) {
                 startTime = System.currentTimeMillis();
@@ -185,6 +190,7 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+            SharedResources.cpuSemaphore.release();
         }
     }
 
